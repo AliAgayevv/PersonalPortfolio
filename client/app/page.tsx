6a13d040c863e876ef49c1c48a58918f05b8d099
aliagayevv <aghayev_233@mail.ru>
2025-06-29 19:34:56 +0400

    [FEAT] - Og images and metadata add

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,12 +10,25 @@ import ProjectSection from "@/sections/ProjectSection";
 import TechStackSection from "@/sections/TechStackSection";
 import TypewriterEffect from "@/components/TypewriterEffect";
 import AnimationWhenElementOnScreen from "@/components/animations/AnimationWhenElementOnScreen";
-import ServiceInterface from "@/types/ServiceInterface";
+import ServiceInterface from "@/types/serviceInterface";
 import getPageData from "@/lib/getPageData";
 import getUrl from "@/lib/getUrl";
 import socialMediaAdress from "@/data/socialMedia.json";
 import Link from "next/link";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
+      {
+        url: `https://aghayev.dev/api/og?title=Portfoliyoma%20xo%C5%9F%20g%C9%99lmisiz`,
+        alt: "Ali's Portfolio",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
 export default async function Home() {
   const cookieStore = await cookies();
   const lang = cookieStore.get("lang")?.value || "az";
